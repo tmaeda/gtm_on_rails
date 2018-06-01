e29b75a77aed457b44f1881d35df58f6f54ee2b7
ykogure <renkin1008@gmail.com>
2018-06-01 15:32:44 +0900

    v0.1.13 Enable ecommerce object add optional args. (#10)

--- lib/gtm_on_rails/models/data_layer/ecommerce.rb
+++ lib/gtm_on_rails/models/data_layer/ecommerce.rb
@@ -3,10 +3,15 @@ module GtmOnRails
   class DataLayer::Ecommerce < DataLayer::Object
     ACTIVITY_TYPES = [:product_impression, :product_click, :product_detail, :add_to_cart, :remove_from_cart, :promotion_impression, :promotion_click, :checkout, :purchase, :refund]
 
-    def initialize(activity_type, **args)
+    def initialize(activity_type, *args)
       raise ArgumentError.new("'#{activity_type}' is undefined activity type.") unless activity_type.in?(ACTIVITY_TYPES)
 
-      @data = send(:"generate_#{activity_type}_hash", args).with_indifferent_access
+      sanitize = true
+      if args.first.keys.count == 1 && args.first.keys.first.to_sym == :sanitize
+        sanitize = args.shift[:sanitize]
+      end
+
+      @data = send(:"generate_#{activity_type}_hash", *[sanitize, args.first]).with_indifferent_access
     end
 
     class << self
@@ -24,214 +29,187 @@ module GtmOnRails
     end
 
     private
-      def generate_product_impression_hash(args)
-        result = {}
 
-        result[:event] = args[:event] if args[:event].present?
+      def generate_product_impression_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:event] = args.delete(:event) if args[:event].present?
 
-        if args[:impressions].present?
-          result[:ecommerce][:impressions] = args[:impressions].map{|impression| impression.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Impression.new(impression) : impression}
-        end
+        result[:ecommerce][:impressions] = get_impression(args) if args[:impressions].present?
+
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_product_click_hash(args)
-        result = {}
+      def generate_product_click_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] || 'productClick'
+        result[:event] = args.delete(:event) || 'productClick'
 
-        result[:event_category] = args[:event_category] || 'Enhanced Ecommerce'
-        result[:event_action]   = args[:event_action]   || 'Product Click'
-        result[:event_label]    = args[:event_label]    || 'Enhanced Ecommerce Product Click'
+        result[:event_category] = args.delete(:event_category) || 'Enhanced Ecommerce'
+        result[:event_action]   = args.delete(:event_action)   || 'Product Click'
+        result[:event_label]    = args.delete(:event_label)    || 'Enhanced Ecommerce Product Click'
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:click]               = {}
+        result[:ecommerce][:click][:actionField] = get_action(args)   if args[:action].present?
+        result[:ecommerce][:click][:products]    = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:click] = {}
-
-        if args[:action].present?
-          action = args[:action].is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(args[:action]) : args[:action]
-          result[:ecommerce][:click][:actionField] = action
-        end
-
-        if args[:products].present?
-          result[:ecommerce][:click][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_product_detail_hash(args)
-        result = {}
+      def generate_product_detail_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] if args[:event].present?
+        result[:event] = args.delete(:event) if args[:event].present?
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:detail]               = {}
+        result[:ecommerce][:detail][:actionField] = get_action(args)   if args[:action].present?
+        result[:ecommerce][:detail][:products]    = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:detail] = {}
-
-        if args[:action].present?
-          action = args[:action].is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(args[:action]) : args[:action]
-          result[:ecommerce][:detail][:actionField] = action
-        end
-
-        if args[:products].present?
-          result[:ecommerce][:detail][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_add_to_cart_hash(args)
-        result = {}
+      def generate_add_to_cart_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] || 'addToCart'
+        result[:event] = args.delete(:event) || 'addToCart'
 
-        result[:event_category] = args[:event_category] || 'Enhanced Ecommerce'
-        result[:event_action]   = args[:event_action]   || 'Add to Cart'
-        result[:event_label]    = args[:event_label]    || 'Enhanced Ecommerce Add to Cart'
+        result[:event_category] = args.delete(:event_category) || 'Enhanced Ecommerce'
+        result[:event_action]   = args.delete(:event_action)   || 'Add to Cart'
+        result[:event_label]    = args.delete(:event_label)    || 'Enhanced Ecommerce Add to Cart'
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:add]            = {}
+        result[:ecommerce][:add][:products] = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:add] = {}
-        if args[:products].present?
-          result[:ecommerce][:add][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_remove_from_cart_hash(args)
-        result = {}
+      def generate_remove_from_cart_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] || 'removeFromCart'
+        result[:event] = args.delete(:event) || 'removeFromCart'
 
-        result[:event_category] = args[:event_category] || 'Enhanced Ecommerce'
-        result[:event_action]   = args[:event_action]   || 'Remove from Cart'
-        result[:event_label]    = args[:event_label]    || 'Enhanced Ecommerce Remove from Cart'
+        result[:event_category] = args.delete(:event_category) || 'Enhanced Ecommerce'
+        result[:event_action]   = args.delete(:event_action)   || 'Remove from Cart'
+        result[:event_label]    = args.delete(:event_label)    || 'Enhanced Ecommerce Remove from Cart'
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:remove]            = {}
+        result[:ecommerce][:remove][:products] = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:remove] = {}
-        if args[:products].present?
-          result[:ecommerce][:remove][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_promotion_impression_hash(args)
-        result = {}
+      def generate_promotion_impression_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] if args[:event].present?
+        result[:event] = args.delete(:event) if args[:event].present?
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:promoView]              = {}
+        result[:ecommerce][:promoView][:promotions] = get_promotion(args) if args[:promotions].present?
 
-        result[:ecommerce][:promoView] = {}
-
-        if args[:promotions].present?
-          result[:ecommerce][:promoView][:promotions] = args[:promotions].map{|promotion| promotion.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Promotion.new(promotion) : promotion}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_promotion_click_hash(args)
-        result = {}
+      def generate_promotion_click_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] || 'promotionClick'
+        result[:event] = args.delete(:event) || 'promotionClick'
 
-        result[:event_category] = args[:event_category] || 'Enhanced Ecommerce'
-        result[:event_action]   = args[:event_action]   || 'Promotion Click'
-        result[:event_label]    = args[:event_label]    || 'Enhanced Ecommerce Promotion Click'
+        result[:event_category] = args.delete(:event_category) || 'Enhanced Ecommerce'
+        result[:event_action]   = args.delete(:event_action)   || 'Promotion Click'
+        result[:event_label]    = args.delete(:event_label)    || 'Enhanced Ecommerce Promotion Click'
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:promoClick]              = {}
+        result[:ecommerce][:promoClick][:promotions] = get_promotion(args) if args[:promotions].present?
 
-        result[:ecommerce][:promoClick] = {}
-
-        if args[:promotions].present?
-          result[:ecommerce][:promoClick][:promotions] = args[:promotions].map{|promotion| promotion.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Promotion.new(promotion) : promotion}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_checkout_hash(args)
-        result = {}
+      def generate_checkout_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] || 'checkout'
+        result[:event] = args.delete(:event) || 'checkout'
 
-        result[:event_category] = args[:event_category] || 'Enhanced Ecommerce'
-        result[:event_action]   = args[:event_action]   || 'Checkout'
-        result[:event_label]    = args[:event_label]    || 'Enhanced Ecommerce Checkout'
+        result[:event_category] = args.delete(:event_category) || 'Enhanced Ecommerce'
+        result[:event_action]   = args.delete(:event_action)   || 'Checkout'
+        result[:event_label]    = args.delete(:event_label)    || 'Enhanced Ecommerce Checkout'
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:checkout]               = {}
+        result[:ecommerce][:checkout][:actionField] = get_action(args)   if args[:action].present?
+        result[:ecommerce][:checkout][:products]    = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:checkout] = {}
-
-        if args[:action].present?
-          action = args[:action].is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(args[:action]) : args[:action]
-          result[:ecommerce][:checkout][:actionField] = action
-        end
-
-        if args[:products].present?
-          result[:ecommerce][:checkout][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_purchase_hash(args)
-        result = {}
+      def generate_purchase_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] if args[:event].present?
+        result[:event] = args.delete(:event) if args[:event].present?
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:purchase]               = {}
+        result[:ecommerce][:purchase][:actionField] = get_action(args)   if args[:action].present?
+        result[:ecommerce][:purchase][:products]    = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:purchase] = {}
-
-        action = args[:action] || {}
-        action = action.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(action) : action
-        result[:ecommerce][:purchase][:actionField] = action
-        
-        if args[:products].present?
-          result[:ecommerce][:purchase][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
       end
 
-      def generate_refund_hash(args)
-        result = {}
+      def generate_refund_hash(sanitize, args)
+        result = get_default_data(args)
 
-        result[:event] = args[:event] if args[:event].present?
+        result[:event] = args.delete(:event) if args[:event].present?
 
-        result[:ecommerce]                = {}
-        result[:ecommerce][:currencyCode] = args[:currency] || GtmOnRails.config.ecommerce_default_currency
+        result[:ecommerce][:refund]               = {}
+        result[:ecommerce][:refund][:actionField] = get_action(args)   if args[:action].present?
+        result[:ecommerce][:refund][:products]    = get_products(args) if args[:products].present?
 
-        result[:ecommerce][:refund] = {}
-
-        if args[:action].present?
-          action = args[:action].is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(args[:action]) : args[:action]
-          result[:ecommerce][:refund][:actionField] = action
-        end
-        
-        if args[:products].present?
-          result[:ecommerce][:refund][:products] = args[:products].map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
-        end
+        result.merge!(args) unless sanitize
 
         return result
+      end
+
+
+      def get_default_data(args)
+        hash = {}
+        hash[:ecommerce]                = {}
+        hash[:ecommerce][:currencyCode] = get_currency(args)
+        hash
+      end
+
+      def get_currency(args)
+        args.delete(:currency) || GtmOnRails.config.ecommerce_default_currency
+      end
+
+      def get_impression(args)
+        args.delete(:impressions).map{|impression| impression.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Impression.new(impression) : impression}
+      end
+
+      def get_promotion(args)
+        args.delete(:promotions).map{|promotion| promotion.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Promotion.new(promotion) : promotion}
+      end
+
+      def get_products(args)
+        args.delete(:products).map{|product| product.is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Product.new(product) : product}
+      end
+
+      def get_action(args)
+        args[:action].is_a?(Hash) ? GtmOnRails::DataLayer::Ecommerce::Action.new(args.delete(:action)) : args.delete(:action)
       end
   end
 end
--- lib/gtm_on_rails/version.rb
+++ lib/gtm_on_rails/version.rb
@@ -1,3 +1,3 @@
 module GtmOnRails
-  VERSION = '0.1.12'
+  VERSION = '0.1.13'
 end
